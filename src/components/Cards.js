7376799663841cb6c317aebcd27cfc4f11e8fb59
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2023-02-05 11:39:02 +0100

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
-import KameliaSource from "../assets/images/kamelia.jpg"
-import CheikhSource from "../assets/images/cheikh.jpg"
 import StephaneSource from "../assets/images/stephane.jpg"
 
 function Cards() {
@@ -13,22 +11,8 @@ function Cards() {
           <ul>
             <CardItem
               src={StephaneSource}
-              text='Data Analyste'
+              text='Data BI Engineer'
               label='Stephane'
-            />
-          </ul>
-          <ul>
-            <CardItem
-              src={KameliaSource}
-              text='Software Engieneer'
-              label='Kamelia'
-            />
-          </ul>
-          <ul>
-            <CardItem
-              src={CheikhSource}
-              text='Data Engieneer'
-              label='Cheikh'
             />
           </ul>
         </div>
