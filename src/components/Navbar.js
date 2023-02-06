be1636038650f09318c0fc73a4592ca591f4868c
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2023-02-06 15:34:17 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,11 +32,6 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/profil' className='navbar-logo' onClick={closeMobileMenu}>
-          KAS
-          <i class='fa fa-music' />
-          </Link>
-          
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
