4a300f5081326facbfe7ae7279552683a225e6df
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2023-02-06 15:57:09 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,41 +29,38 @@ function Navbar() {
   window.addEventListener('resize', showButton);
 
   return (
-    <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <div className='menu-icon' onClick={handleClick}>
+          <div Link to="/" className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            
             <li className='nav-item'>
               <Link to="/TranscriptedAudio" className='nav-links' onClick={closeMobileMenu}>
               Bibliothèque
               </Link>
            </li>
+
            <li className='nav-item'>
               <Link to="/" className='nav-links' onClick={closeMobileMenu}>
               Home
               </Link>
            </li>
+
            <li className='nav-item'>
-              <Link
-                to='/contact'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+              <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
                 Contactez-Nous
               </Link>
-            </li>
+           </li>
+
             <li>
-              <Link
-                to='/contact'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
+              <Link to='/contact' className='nav-links-mobile' onClick={closeMobileMenu}>
                  KAS : l'équipe !
               </Link>
             </li>
+
           </ul>
           {button && <Button buttonStyle='btn--outline' path="/contact"> KAS : l'équipe ! <i className='far fa-play-circle' /></Button>}
         </div>
