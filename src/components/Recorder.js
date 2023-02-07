629b8dad80c91f0ee5000fe4473ee926584de9c9
Stephanenzb <36003820+Stephanenzb@users.noreply.github.com>
2023-02-07 14:22:08 +0100

    Update Recorder.js

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -131,7 +131,7 @@ const Recorder = () =>{
         const id = Math.floor(Math.random() * 100)
        
         axios.post(
-            "https://kasapi-dot-metal-repeater-352000.uc.r.appspot.com/store-transcription/bdd_kas_transcript", "bdd_kas_transcript",
+            "https://kasapi-dot-lastkas.ey.r.appspot.com/store-transcription/bdd_kas_transcript", "bdd_kas_transcript",
               {
                   params: {
                       title:id,
@@ -155,7 +155,7 @@ const Recorder = () =>{
 
     const transcript = () =>{
         axios.get(
-            "https://kasapi-dot-metal-repeater-352000.uc.r.appspot.com/transcription", {
+            "https://kasapi-dot-lastkas.ey.r.appspot.com/transcription", {
                 params : {
                     public_url: url
                 }
@@ -184,7 +184,7 @@ const Recorder = () =>{
 
     const getData = () =>{
         return axios.get(
-            "https://kasapi-dot-metal-repeater-352000.uc.r.appspot.com/stored_transcriptions", {
+            "https://kasapi-dot-lastkas.ey.r.appspot.com/stored_transcriptions", {
                 params : {
                     index_name : "bdd_kas_transcript" 
                 }
